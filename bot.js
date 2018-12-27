064ad16ecac1790aa83a82e206cbb0e82246ef7c
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-12-27 15:23:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -354,7 +354,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`&play |&help|BY ~ Unknown ~Tag~ #4403|`,"http://twitch.tv/Death Shop")
+client.user.setGame(`&play |&help|BY~Yasser AlQahtani|`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
