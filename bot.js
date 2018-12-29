b8c8c7acc95ed192d7016611399b622232569081
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-12-29 22:59:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -447,8 +447,8 @@ client.on('message', message => { //Toxic Codes
 
 client.on('ready',async () => {//Toxic Codes
 console.log("Starting..");//Toxic Codes
-let g = client.guilds.get("528649421974405120" , "274665615833432064"); // id server
-let c = g.channels.get("528649421974405130" , "344179333469962242");// id channel
+let g = client.guilds.get("528649421974405120"); // id server
+let c = g.channels.get("528649421974405130");// id channel
 if(c.type === 'voice') {//Toxic Codes
 c.join();//Toxic Codes
 setInterval(() => {//Toxic Codes
@@ -458,6 +458,23 @@ if(!g.me.voiceChannel) c.join();
 console.log("Failed To Join:\n The Channel Type isn't \"text\"");
 }//Toxic Codes
 });
+
+
+client.on('ready',async () => {//Toxic Codes
+console.log("Starting..");//Toxic Codes
+let g = client.guilds.get("274665615833432064"); // id server
+let c = g.channels.get("344179333469962242");// id channel
+if(c.type === 'voice') {//Toxic Codes
+c.join();//Toxic Codes
+setInterval(() => {//Toxic Codes
+if(!g.me.voiceChannel) c.join();
+}, 1);//Toxic Codes
+} else {//Toxic Codes
+console.log("Failed To Join:\n The Channel Type isn't \"text\"");
+}//Toxic Codes
+});
+
+
 
 
 client.login(process.env.BOT_TOKEN);
